51565a4832d2cff7c92bc80892a13b01d317d7c0
Youngro Kim <rich@imac>
2016-02-24 22:03:21 -0800

    update

--- MomsCounter/main.cpp
+++ MomsCounter/main.cpp
@@ -7,12 +7,13 @@
 using namespace std;
 #include "MomsCounter.h"
 
-// function: get user choices key stroke
+// input function
 void getUserChoice(char &key);
 
 int main() {
     bool b_error = false;
     char ans;
+    // initialize MomsCounter object
     MomsCounter obj_count;
     
     do
@@ -32,41 +33,51 @@ int main() {
             getUserChoice(ans);
         }
         
-        
         if (ans == 'a' || ans == 'A') {
+            // call MomsCounter member function
             obj_count.IncrementThousand();
+            // verify after increment
             if (obj_count.getCount() <= 0) {
                 cout << "Increment by thousand failed";
                 exit(1);
             }
         }
         else if (ans == 's' || ans == 'S') {
+            // call MomsCounter member function
             obj_count.IncrementHundred();
+            // verify after increment
             if (obj_count.getCount() <= 0) {
                 cout << "Increment by hundred failed";
                 exit(1);
             }
         }
         else if (ans == 'd' || ans == 'D') {
+            // call MomsCounter member function
             obj_count.IncrementTen();
+            // verify after increment
             if (obj_count.getCount() <= 0) {
                 cout << "Increment by ten failed";
                 exit(1);
             }
         }
         else if (ans == 'f' || ans == 'F') {
+            // call MomsCounter member function
             obj_count.IncrementOne();
+            // verify after increment
             if (obj_count.getCount() <= 0) {
                 cout << "Increment by one failed";
                 exit(1);
             }
         }
         else if (ans == 'r' || ans == 'R') {
+            // call MomsCounter output member function
             obj_count.output(cout);
+            // reset the counter to 0
             obj_count.Clear();
             cout << "Counter has been reset" << endl;
         }
         else if (ans == 'x' || ans == 'X') {
+            // exit program when user entered x or X key
             exit(1);
         }
         else {
@@ -74,11 +85,12 @@ int main() {
             b_error = true;
         }
         
-    } while(1);
+    } while(1); // keep looping
     
     return 0;
 }
 
+// function: get user choices key stroke
 void getUserChoice(char &key) {
     cin >> key;
 }
